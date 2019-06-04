a16fe7b7465c4ef09a93c51b30e5dfdddb0fce7f
Alex Siler <davidalexsiler@gmail.com>
2019-06-04 16:51:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,8 +10,8 @@ def get_first_name_of_season_winner(data, season)
           winner = info['status']['name']
         end
       end
-          binding.pry
         winner
+          binding.pry
     end
   end
 end

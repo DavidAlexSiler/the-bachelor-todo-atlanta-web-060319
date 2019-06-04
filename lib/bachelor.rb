2ecb075bf47d1070a2598e79437ff9a4cd67036d
Alex Siler <davidalexsiler@gmail.com>
2019-06-04 16:48:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,6 +8,9 @@ def get_first_name_of_season_winner(data, season)
       sn.each do |info|
         if info['status'] == 'Winner'
           puts info['status']['name']
+        end
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
